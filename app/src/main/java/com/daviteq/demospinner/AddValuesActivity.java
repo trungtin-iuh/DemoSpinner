3aec005fb7f6f8b057fc6436bab43a49ecd932ad
TrungTin <trungtin.iuh>
2018-05-05 11:54:29 +0700

    fix
    update 5/5/2018

--- app/src/main/java/com/daviteq/demospinner/AddValuesActivity.java
+++ app/src/main/java/com/daviteq/demospinner/AddValuesActivity.java
@@ -1,18 +1,26 @@
 package com.daviteq.demospinner;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class AddValuesActivity extends AppCompatActivity {
 
     private EditText edtValues;
     private Button btnAdd,btnBack;
+
+    private List<String> listValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,14 +44,36 @@ public class AddValuesActivity extends AppCompatActivity {
         });
     }
 
+
     private void processAddValues() {
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+//                listValues = new ArrayList<>();
                 String value = edtValues.getText().toString();
-                Add add = new Add();
-                add.setValues(new String[]{value});
+//                listValues.addAll(Collections.singleton(value));
+
+//                Set<String> set = new HashSet<String>();
+//                set.addAll(listValues);
+//                StringBuilder sb = new StringBuilder();
+//
+//                for (String s : listValues){
+//                    sb.append(s);
+//                    sb.append(",");
+//                }
+
+
+
+                SharedPreferences sharePref = getSharedPreferences("PREFS",0);
+                SharedPreferences.Editor editor = sharePref.edit();
+                Set<String> set = new HashSet<String>();
+//                Set<String> set = sharePref.getStringSet("listValues", new HashSet<String>());
+                set.addAll(Collections.singleton(value));
+                editor.putStringSet("listValues",set);
+                editor.apply();
+
                 edtValues.setText("");
+
             }
         });
     }
--- app/src/main/java/com/daviteq/demospinner/MainActivity.java
+++ app/src/main/java/com/daviteq/demospinner/MainActivity.java
@@ -2,8 +2,10 @@ package com.daviteq.demospinner;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Adapter;
@@ -19,14 +21,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private Spinner spListValues;
     private Button btnShow;
     private Dialog dialog;
+    private ArrayList<String> val;
+    ArrayAdapter<String> arr = null;
 
-    private String[] val = new String[]{};
+//    private String[] val = new String[]{};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,41 +41,68 @@ public class MainActivity extends AppCompatActivity {
         spListValues = findViewById(R.id.spListValues);
         btnShow = findViewById(R.id.showDialog);
 
-        final List<String> listValues = new ArrayList<>(Arrays.asList(val));
-        final ArrayAdapter<String> arr = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line,listValues);
-        spListValues.setAdapter(arr);
+        try {
+
+            SharedPreferences sharePref = getSharedPreferences("PREFS",0);
+            Set<String> set = sharePref.getStringSet("listValues",null);
+
+            val = new ArrayList<>();
+
+            val.addAll(set);
+//            for (int i = 0; i < set.size(); i++){
+//                val.add(set[i]);
+//            }
+//            String valuesString = sharePref.getString("listValues","");
+//            String[] itemList =  valuesString.split(",");
+//            List<String> listValues = new ArrayList<>();
+//
+//            for (int i = 0; i < itemList.length; i++){
+//                listValues.addAll(Collections.singleton(itemList[i]));
+//            }
+//
+//            for (int i = 0; i < listValues.size(); i++){
+//                val = new ArrayList<>();
+//                val.addAll(Collections.singleton(listValues.get(i)));
+//            }
+//
+            arr = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, val);
+            spListValues.setAdapter(arr);
+
+        } catch (Exception e){
+            Log.w("Exception", e.toString());
+        }
 
         btnShow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,AddValuesActivity.class);
-//                startActivity(intent);
-                dialog = new Dialog(MainActivity.this);
-                dialog.setContentView(R.layout.dialog_add);
-                final EditText edtValues = dialog.findViewById(R.id.edtValues);
-                Button btnAdd = dialog.findViewById(R.id.btnAdd);
-
-                Button btnBack = dialog.findViewById(R.id.btnBack);
-
-                btnBack.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Toast.makeText(MainActivity.this,"Successfully",Toast.LENGTH_SHORT).show();
-                        dialog.dismiss();
-                    }
-                });
-
-                btnAdd.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String values = edtValues.getText().toString();
-                        listValues.addAll(Collections.singleton(values));
-                        edtValues.setText("");
-                        arr.notifyDataSetChanged();
-                    }
-                });
-
-                dialog.show();
+                Intent intent = new Intent(MainActivity.this,AddValuesActivity.class);
+                startActivity(intent);
+//                dialog = new Dialog(MainActivity.this);
+//                dialog.setContentView(R.layout.dialog_add);
+//                final EditText edtValues = dialog.findViewById(R.id.edtValues);
+//                Button btnAdd = dialog.findViewById(R.id.btnAdd);
+//
+//                Button btnBack = dialog.findViewById(R.id.btnBack);
+//
+//                btnBack.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        Toast.makeText(MainActivity.this,"Successfully",Toast.LENGTH_SHORT).show();
+//                        dialog.dismiss();
+//                    }
+//                });
+//
+//                btnAdd.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View v) {
+//                        String values = edtValues.getText().toString();
+//                        listValues.addAll(Collections.singleton(values));
+//                        edtValues.setText("");
+//                        arr.notifyDataSetChanged();
+//                    }
+//                });
+//
+//                dialog.show();
             }
         });
 
@@ -93,8 +125,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-//    public void dataAddAdapter(String text){
-//        adapter.add(text);
+//    public void dataAddAdapter(List<String> values){
+//        adapter.addAll(values);
 //        adapter.notifyDataSetChanged();
 //    }
 }
